426e3bb75127bb7a47f1e244fa6adc5b7bb3233f
Shadypio <enzuccioamendola@gmail.com>
2023-02-03 16:01:47 +0100

    Added hero name in message

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -23,7 +23,7 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id} - ${hero.name}`);
   }
 
   getHeroes(): void {
