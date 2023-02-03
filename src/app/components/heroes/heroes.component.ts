d36fc362ebdb5b7aa40787f5ff9dd9f3919d1390
Shadypio <enzuccioamendola@gmail.com>
2023-02-03 15:51:41 +0100

    Messages to keep track history of selected heroes

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from 'src/app/hero';
 import { HeroService } from 'src/app/services/hero.service';
+import { MessageService } from 'src/app/services/messages.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,26 +11,23 @@ import { HeroService } from 'src/app/services/hero.service';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes: Hero[] = [];
   selectedHero?: Hero;
 
-  constructor(private heroService: HeroService) { }
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero) {
+  onSelect(hero: Hero): void {
     this.selectedHero = hero;
-
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
     this.heroService.getHeroes()
         .subscribe(heroes => this.heroes = heroes);
   }
-
-
-
 }
-
