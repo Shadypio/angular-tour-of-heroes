6c5500eb80d4a615517d44245df8b486f457976a
Shadypio <enzuccioamendola@gmail.com>
2023-02-03 17:32:35 +0100

    Added simple dashboard with heroes table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
-import { MessagesComponent } from './componets/messages/messages.component';
+import { MessagesComponent } from './components/messages/messages.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
